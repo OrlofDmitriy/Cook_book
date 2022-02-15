910ece9add47186a564e6b806e857e02642a0bd9
Дмитрий Орлов <orlof@mail.ru>
2022-02-15 19:34:31 +0500

    Task.2. Added a function to get a shopping list

--- main.py
+++ main.py
@@ -11,5 +11,21 @@ def get_cook_book(recipes_file='recipes.txt'):
     return cook_book
 
 
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            new_ingredient = dict(ingredient)
+            new_ingredient['quantity'] *= person_count
+            if new_ingredient['ingredient_name'] not in shop_list:
+                shop_list[new_ingredient['ingredient_name']] = new_ingredient
+            else:
+                shop_list[new_ingredient['ingredient_name']]['quantity'] += new_ingredient['quantity']
+    return shop_list
+
+
 cook_book = get_cook_book()
 print(cook_book)
+print()
+shop_list = get_shop_list_by_dishes(['Омлет', 'Омлет'], 1)
+print(shop_list)
